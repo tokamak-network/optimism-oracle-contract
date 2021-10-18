fdd5bb1bdc04b89ac8504b9e31f31259db91a81b
sgnoo <dn2757@gmail.com>
2021-10-18 21:38:40 +0900

    Fix ERC721 compile issue on L2

--- contracts/L1ClaimableToken.sol
+++ contracts/L1ClaimableToken.sol
@@ -1,71 +1,69 @@
-// // SPDX-License-Identifier: MIT
-// // @unsupported: ovm
-// pragma solidity >=0.7.6;
-// pragma abicoder v2;
+// SPDX-License-Identifier: MIT
+// @unsupported: ovm
 
-// import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { ERC721 } from "./ERC721.sol";
 
-// contract L1ClaimableToken is ERC721 {
+contract L1ClaimableToken is ERC721 {
 
-//     event Minted(uint256 indexed tokenId, uint256 indexed l2TxIndex);
+    event Minted(uint256 indexed tokenId, uint256 indexed l2TxIndex);
 
-//     struct TokenInfo {
-//         uint256 tokenId;
-//         uint256 transactionIndex;
-//         address origin;
-//         address l1Token;
-//         address l2Token;
-//         uint256 amount;
-//         uint256 fee;
-//         bool claimed;
-//     }
+    struct TokenInfo {
+        uint256 tokenId;
+        uint256 transactionIndex;
+        address origin;
+        address l1Token;
+        address l2Token;
+        uint256 amount;
+        uint256 fee;
+        bool claimed;
+    }
 
-//     mapping(uint256 => TokenInfo) public tokenInfos;
-//     address public owner;
+    mapping(uint256 => TokenInfo) public tokenInfos;
+    address public owner;
 
-//     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
-//         owner = msg.sender;
-//     }
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        owner = msg.sender;
+    }
 
-//     function transferOwnership (address newOwner) public {
-//         require(msg.sender == owner, "NOT_OWNER");
-//         owner = newOwner;
-//     }
+    function transferOwnership (address newOwner) public {
+        require(msg.sender == owner, "NOT_OWNER");
+        owner = newOwner;
+    }
 
-//     function mint(
-//         uint256 tokenId,
-//         uint256 transactionIndex,
-//         address origin,
-//         address l1Token,
-//         address l2Token,
-//         uint256 amount,
-//         uint256 fee
-//     )
-//         external
-//     {
-//         require(msg.sender == owner, "NOT_OWNER");
+    function mint(
+        uint256 tokenId,
+        uint256 transactionIndex,
+        address origin,
+        address l1Token,
+        address l2Token,
+        uint256 amount,
+        uint256 fee
+    )
+        external
+    {
+        require(msg.sender == owner, "NOT_OWNER");
 
-//         _mint(msg.sender, tokenId);
+        _mint(msg.sender, tokenId);
 
-//         TokenInfo storage tokenInfo = tokenInfos[tokenId];
-//         tokenInfo.tokenId = tokenId;
-//         tokenInfo.transactionIndex = transactionIndex;
-//         tokenInfo.origin = origin;
-//         tokenInfo.l1Token = l1Token;
-//         tokenInfo.l2Token = l2Token;
-//         tokenInfo.amount = amount;
-//         tokenInfo.fee = fee;
-//         tokenInfo.claimed = false;
+        TokenInfo storage tokenInfo = tokenInfos[tokenId];
+        tokenInfo.tokenId = tokenId;
+        tokenInfo.transactionIndex = transactionIndex;
+        tokenInfo.origin = origin;
+        tokenInfo.l1Token = l1Token;
+        tokenInfo.l2Token = l2Token;
+        tokenInfo.amount = amount;
+        tokenInfo.fee = fee;
+        tokenInfo.claimed = false;
 
-//         emit Minted(tokenId, transactionIndex);
-//     }
+        emit Minted(tokenId, transactionIndex);
+    }
 
-//     function claim (uint256 tokenId) public {
-//         require(
-//             msg.sender == owner,
-//             "NO_OWNERSHIP"
-//         );
-//         TokenInfo storage tokenInfo = tokenInfos[tokenId];
-//         tokenInfo.claimed = true;
-//     }
-// }
+    function claim (uint256 tokenId) public {
+        require(
+            msg.sender == owner,
+            "NO_OWNERSHIP"
+        );
+        TokenInfo storage tokenInfo = tokenInfos[tokenId];
+        tokenInfo.claimed = true;
+    }
+}
